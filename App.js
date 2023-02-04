117198dd89935f37cab9ead203de39365f97622a
davidkasemervisz <124424109+davidkasemervisz@users.noreply.github.com>
2023-02-03 22:55:39 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import AppLoading from "expo-app-loading";
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Image, YellowBox } from "react-native";
 import { useFonts } from "expo-font";
-import { Themes } from "./assets/Themes";
+import { Profiles, Themes } from "./assets/Themes";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Footer } from "./app/components";
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
+import { App_Profiles } from "./assets/Profiles/index";
+import themes from "./assets/Themes/themes";
+import pallet from "./assets/Themes/palette"
+import Icons from "./assets/Themes";
+import symbolicateStackTrace from "react-native/Libraries/Core/Devtools/symbolicateStackTrace";
 
 /* Keep the splash screen visible while we fetch resources */
 SplashScreen.preventAutoHideAsync();
@@ -15,24 +20,46 @@ SplashScreen.preventAutoHideAsync();
  * learn more about in the coming weeks!
  */
 function HomeScreen() {
-  /* TODO: insert your code here */
+
+  var profile = App_Profiles.mtl;
 
   return (
     <View style={styles.container}>
-      <Text
-        style={{
-          fontFamily: "Sydney", // test to see if the font is loaded, feel free to remove this
-        }}
-      >
-        Open up App.js to start working on your app!
-      </Text>
-      <Text
-        style={{
-          fontFamily: "Sydney-Bold", // test to see if the font is loaded, feel free to remove this
-        }}
-      >
-        ~Good luck~
-      </Text>
+      <View style={styles.header}>
+        <Image resizeMode = "contain"
+          style={{ height: "100%" }}
+          source={require("./assets/Icons/menu_light.png")} />
+        <Text style={{ fontFamily: "Sydney-Bold", fontSize: 48 }}>
+          ensom
+        </Text>
+        <Image resizeMode="contain"
+          style={{ height: "100%" }}
+          source={require("./assets/Icons/sun.png")} />
+      </View>
+      <View style={styles.main}>
+        <View style = {styles.image_box}>
+          <Image resizeMode = "stretch"
+            style = { [styles.image_box, {borderRadius: 20, position: "relative"}] }
+            source={profile.image} />
+          <Text style = {{ fontFamily: "Sydney-Bold", fontSize: 48, position: "absolute", top: "2.5%", start: "2.5%" }}>
+            {profile.name}
+          </Text>
+          <Text style={{ fontFamily: "Sydney", fontSize: 24, position: "absolute", top: "92.5%", start: "2.5%" }}>
+            {profile.caption}
+          </Text>
+        </View>
+        <View style = {styles.audio_box}>
+          <Text style = {{ fontFamily: "Sydney", fontSize: 36, start: "2.5%" }}>
+            My Hottest Take
+           </Text>
+           <Image resizeMode = "contain"
+          style = {{ height: "50%", position: "absolute", top: "25%", start: "-10%" }}
+           source = {require("./assets/Icons/player_light.png")} />
+           <Image resizeMode = "contain"
+           style = {{top: "15%", heigth: "52.5%", width: "70%", position: "absolute", start: "30%"}}
+          source = {require("./assets/Icons/audio_waveform_light.png")} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -75,8 +102,58 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    flexDirection: "column",
+    backgroundColor: themes.light.backgroundColor,
     alignItems: "center",
     justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#BFBFBF",
+  },
+
+  header: {
+    flex: 2,
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    position: "absolute",
+    top: "5%",
+    start: 0,
+    width: "100%",
+    height: "8%",
+  },
+
+  main: {
+    flex: 2,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 4,
+    width: "100%",
+    top: "13%",
+    start: 0,
+    width: "100%",
+    height: "82%",
+    position: "absolute",
+  },
+
+  audio_box: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    flex: 3,
+    top: "72%",
+    width: "100%",
+    height: "32.5%",
+    position: "absolute",
+    padding: 4,
+  },
+
+  image_box: {
+    flex: 3,
+    width: "100%", 
+    height: "67.5%",
+    top: "2%", 
+    position: "absolute",
   },
 });
